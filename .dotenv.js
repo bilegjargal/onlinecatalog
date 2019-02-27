455d93d8399d21ca043726c654069040d90d55b4
bilgeejargalsaihan <bilgee.py@gmail.com>
2019-02-27 09:51:07 -0600

    mongo client connection

---
+++ .dotenv.js
@@ -0,0 +1,5 @@
+const config = {
+  "dbConnection": "mongodb://admin:t12@xmcnm@ds255005.mlab.com:55005/ecatalog"
+};
+
+exports.config = config;
--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 const express = require('express');
+const parser = require('body-parser');
+const mongo = require('mongodb').MongoClient;
+const config = require("./.dotenv");
 
+let db = null;
 app = express();
+mongo.connect(config.config.dbConnection, function (err, client) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  db = client.db("ecatalog");
+});
+
 app.listen(8080, () => console.log("server started listening on 8080"));
