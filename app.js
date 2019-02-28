6ad9b7dad9894a845502268f584f011973abd956
bilgeejargalsaihan <bilgee.py@gmail.com>
2019-02-27 21:52:54 -0600

    login part

--- app.js
+++ app.js
@@ -1,11 +1,41 @@
 const express = require('express');
 const parser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const config = require("./dotenv");
 
-let db = null;
 app = express();
-mongoose.connect(config.config.dbConnection, { useNewUrlParser: true }, function (err) {
-  if (err) console.log(err);
-})
+//1. mongoose
+mongoose.connect(config.config.dbConnection, { useNewUrlParser: true },
+  function (err, client) {
+    if (err) console.log(err);
+  });
+let db = mongoose.connection;
+app.use(parser.json());
+app.use(parser.urlencoded({ extended: true }));
+app.use(cors());
+app.use((req, res, next) => {
+  req.db = db;
+  next();
+});
+
+//2. login
+app.post("/login", async (req, res) => {
+  let user = undefined;
+  await req.db.model("user")
+    .find({ username: req.body.uname }).forEach(data => {
+      if (compare(req.body.password, data.password))
+        user = data;
+    });
+  if (user) {
+    const token = jwt.sign({
+      name: user.username,
+    }, "secret", { expiresIn: "2h" });
+    res.header('Authorization', `Bearer ${token}`);
+    res.json({ success: true, token: token });
+  }
+  res.json({ success: false });
+});
+
 app.listen(8080, () => console.log("server started listening on 8080"));
